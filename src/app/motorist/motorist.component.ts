37ad98338828133a8b5d060b489d05a898ad06fe
Elton Ventura <elton.ea@live.com>
2018-02-09 00:56:38 -0200

    feat: add motorist component

---
+++ src/app/motorist/motorist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sga-motorist',
+  templateUrl: './motorist.component.html',
+  styles: []
+})
+export class MotoristComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
