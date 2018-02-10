19ad195691bceea1d2d3dac4b63fd5c8b2707648
Elton Ventura <elton.ea@live.com>
2018-02-10 00:21:29 -0200

    feat: add shared module

--- src/app/motorist/motorist.module.ts
+++ src/app/motorist/motorist.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { MotoristRoutingModule } from './motorist-routing.module';
 import { MotoristComponent } from './motorist.component';
+import { SharedModule } from '../shared/modules/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule,
+    SharedModule,
     MotoristRoutingModule
   ],
   declarations: [MotoristComponent]
