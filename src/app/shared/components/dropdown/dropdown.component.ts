10e879cbe6593928a317320074699dbf4572385f
Elton Ventura <elton.ea@live.com>
2018-02-10 00:22:09 -0200

    feat: add dropdown component

--- src/app/shared/components/dropdown/dropdown.component.ts
+++ src/app/shared/components/dropdown/dropdown.component.ts
@@ -1,20 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { DropdownBodyComponent } from './dropdown-body/dropdown-body.component';
+import { DropdownHeaderComponent } from './dropdown-header/dropdown-header.component';
 
 @Component({
   selector: 'sga-dropdown',
   templateUrl: './dropdown.component.html',
   styleUrls: ['./dropdown.component.scss']
 })
-export class DropdownComponent implements OnInit {
-
+export class DropdownComponent {
+  public body: DropdownBodyComponent;
+  public header: DropdownHeaderComponent;
   public menuOpen = false;
-  
-  constructor() { }
 
-  ngOnInit() {
-  }
+  constructor() {}
 
   onHeaderClick() {
+    this.menuOpen = !this.menuOpen;
+  }
 
+  setHeader(header: DropdownHeaderComponent) {
+    this.header = header;
+  }
+
+  setBody(body: DropdownBodyComponent) {
+    this.body = body;
   }
 }
