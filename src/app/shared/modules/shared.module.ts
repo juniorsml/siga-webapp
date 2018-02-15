9fa9e1a17841f529d8ba812f9e343a91da9fb6b2
Elton Ventura <elton.ea@live.com>
2018-02-15 00:35:29 -0200

    feat: add shared tabs component

--- src/app/shared/modules/shared.module.ts
+++ src/app/shared/modules/shared.module.ts
@@ -4,9 +4,13 @@ import { CommonModule } from '@angular/common';
 import { DropdownComponent } from '../components/dropdown/dropdown.component';
 import { DropdownBodyComponent } from '../components/dropdown/dropdown-body/dropdown-body.component';
 import { DropdownHeaderComponent } from '../components/dropdown/dropdown-header/dropdown-header.component';
+import { TabsComponent } from '../components/tabs/tabs.component';
+import { TabComponent } from '../components/tabs/tab/tab.component';
 
 @NgModule({
   declarations: [
+    TabComponent,
+    TabsComponent,
     DropdownComponent,
     DropdownBodyComponent,
     DropdownHeaderComponent
@@ -15,6 +19,8 @@ import { DropdownHeaderComponent } from '../components/dropdown/dropdown-header/
     CommonModule
   ],
   exports: [
+    TabComponent,
+    TabsComponent,
     DropdownComponent,
     DropdownBodyComponent,
     DropdownHeaderComponent
