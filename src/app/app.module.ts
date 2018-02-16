889d4a90b7f58d2e5c5b6be2f934b2d93a8bf359
Elton Ventura <elton.ea@live.com>
2018-02-16 02:08:00 -0200

    feat: add Layout components in module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,19 @@ import { httpFactory } from './shared/factory/http.factory';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './shared/layout/navbar/navbar.component';
+import { SidebarComponent } from './shared/layout/sidebar/sidebar.component';
+import { ContentComponent } from './shared/layout/content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    NavbarComponent,
+    SidebarComponent,
+    ContentComponent,
   ],
   imports: [
     FormsModule,
