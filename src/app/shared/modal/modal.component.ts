ff71360ec8bbdd5df87a904a3386ef5ce91b3093
Elton Ventura <elton.ea@live.com>
2018-02-16 02:13:25 -0200

    feat: add modal component

---
+++ src/app/shared/modal/modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sga-modal',
+  templateUrl: './modal.component.html',
+  styles: []
+})
+export class ModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
