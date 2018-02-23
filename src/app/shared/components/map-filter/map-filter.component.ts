e58dffadeb1c85745e555f4e8b7a4e0383532662
Elton Ventura <elton.ea@live.com>
2018-02-23 02:57:32 -0300

    feat: add shared map filter component

---
+++ src/app/shared/components/map-filter/map-filter.component.ts
@@ -0,0 +1,72 @@
+import {
+  Component,
+  Input,
+  Output,
+  ViewChild,
+  EventEmitter,
+  ElementRef,
+  NgZone,
+  AfterViewInit
+} from '@angular/core';
+
+@Component({
+  selector: 'sga-map-filter',
+  templateUrl: './map-filter.component.html'
+})
+export class MapFilterComponent implements AfterViewInit {
+  @Input() styleClass: string;
+  @Output('placesFiltered') public placesFilteredEvent: EventEmitter<any>;
+  @Output('filterRemoved') public filterRemoved: EventEmitter<any>;
+  @ViewChild('input') public input: ElementRef;
+  placesInput: any;
+  distance: any;
+  location: any;
+  placeText: string;
+
+  constructor(private ngZone: NgZone) {
+    this.placesFilteredEvent = new EventEmitter();
+    this.filterRemoved = new EventEmitter();
+    // this.placesInput = new google.maps.places.Autocomplete(<HTMLInputElement> document.getElementById('#mapFilter'));
+    this.placesInput.addListener('place_changed', () => this.setLocation());
+  }
+
+  setLocation() {
+    this.ngZone.run(() => {
+      this.location = this.placesInput.getPlace().geometry.location;
+      this.setDistanceAndLocation();
+    });
+  }
+
+  onDistanceKeyUp() {
+    this.setLocation();
+  }
+
+  onPlacesKeyUp() {
+    this.location = null;
+    this.filterRemoved.emit();
+  }
+
+  setDistanceAndLocation() {
+    if (
+      !isNaN(this.distance) &&
+      this.distance !== '' &&
+      this.location !== 'undefined' &&
+      this.location !== null
+    ) {
+      this.placesFilteredEvent.emit({
+        lat: this.location.lat(),
+        lng: this.location.lng(),
+        distance: this.distance
+      });
+    } else {
+      this.filterRemoved.emit();
+    }
+  }
+
+  ngAfterViewInit() {
+    // Initialize places input
+    // var nativeElement: HTMLInputElement = this.input.nativeElement;
+    // this.placesInput = new google.maps.places.Autocomplete(nativeElement);
+    this.placesInput.addListener('place_changed', () => this.setLocation());
+  }
+}
