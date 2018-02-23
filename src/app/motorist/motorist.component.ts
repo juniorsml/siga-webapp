8cfab911b62403a16f763b5cd30f3c52f6199fa9
Elton Ventura <elton.ea@live.com>
2018-02-23 02:57:48 -0300

    feat: add motorist component

--- src/app/motorist/motorist.component.ts
+++ src/app/motorist/motorist.component.ts
@@ -1,18 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { TabComponent } from '../shared/components/tabs/tab/tab.component';
 
 @Component({
   selector: 'sga-motorist',
   templateUrl: './motorist.component.html',
   styles: []
 })
-export class MotoristComponent implements OnInit {
-
+export class MotoristComponent {
   motorists: any;
+  mapHasFocus: boolean;
+  selectedTabIndex: number;
   showAssociateModal: boolean;
-  
-  constructor() { }
 
-  ngOnInit() {
+  onTabSelected(tab: TabComponent) {
+    switch (tab.index) {
+      case 0:
+        this.mapHasFocus = false;
+        break;
+      case 1:
+        this.mapHasFocus = true;
+        break;
+    }
+    this.selectedTabIndex = tab.index;
   }
-
 }
