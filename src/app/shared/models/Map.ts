535c1dcdf5dcc57181311c9aa2f730a0f6de56e7
Elton Ventura <elton.ea@live.com>
2018-02-27 01:57:57 -0300

    feat: add Map abstract class

---
+++ src/app/shared/models/Map.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { Feature } from 'geojson';
+/**
+ * Created by davidherod on 19/5/17.
+ */
+
+@Injectable()
+export abstract class Map {
+  
+  public abstract moveTo(
+    latitude: number,
+    longitude: number,
+    options?: any
+  ): void;
+  
+  public abstract setCenter(latitude: number, longitude: number): void;
+  
+  public abstract getContainer(): HTMLElement;
+
+  public abstract append(container: HTMLElement): void;
+
+  public abstract resize(): void;
+
+  public abstract setZoom(level: number): void;
+
+  public abstract setBounds(bounds: any): void;
+
+  public abstract setBoundsByMarkers(): void;
+
+  /** Shared **/
+  public abstract clearAll(): void;
+
+  /** Marker **/
+
+  public abstract createMarker(feature: Feature<any>): any;
+
+  public abstract addMarker(feature: Feature<any>): any;
+
+  public abstract addMarkerPopUp(marker: any, text: string): any;
+
+  public abstract addSource(id?: any, source?: any): void;
+
+  public abstract addCluster(markers: Array<any>): void;
+
+  public abstract removeCluster(): void;
+
+  public abstract removeClusters(): void;
+
+  public abstract clearMarkers(): void;
+
+  /** Drawing **/
+
+  public abstract drawPolyline(features: Array<Feature<any>>): void;
+}
