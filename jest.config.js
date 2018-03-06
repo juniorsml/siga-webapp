24c330fcb7f81c8e8153a42687f9bec88284d6a7
Valter Gomes <Valter@condusit.com>
2018-03-06 15:23:04 -0300

    Chore: Add a report to the tests
    Add a plugin which generetes a html page to unit tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'jest-preset-angular',
   setupTestFrameworkScriptFile: './src/setup-jest.ts',
+  "testResultsProcessor": "./node_modules/jest-html-reporter",	
   coverageReporters: [
     'html', 'text'
   ]
