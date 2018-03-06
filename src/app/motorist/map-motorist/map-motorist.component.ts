1232bdf90f0fefe34c53db42513b4a2c50300c20
Elton Ventura <elton.ea@live.com>
2018-03-06 18:37:40 -0300

    feat: add tab hooks

--- src/app/motorist/map-motorist/map-motorist.component.ts
+++ src/app/motorist/map-motorist/map-motorist.component.ts
@@ -8,19 +8,45 @@ import {
 import {} from "leaflet-marker-cluster"
 
 import { Map } from '../../shared/models/Map';
+import { TabComponent } from '../../shared/components/tabs/tab/tab.component';
 
 @Component({
   selector: 'sga-map-motorist',
-  templateUrl: './map-motorist.component.html'
+  templateUrl: './map-motorist.component.html',
+  styleUrls: ['./map-motorist.component.scss']
 })
 export class MapMotoristComponent implements OnInit {
+
   @ViewChild('mapSelector') mapSelector: ElementRef;
-  
+
+  motorists = new Array();
+  mapLocationHistory = new Array();
+
+  mapSearchText: any;
+  mapSelectedTabIndex: number;
+  mapTabsSelectedIndex: number;
+
   constructor(private map: Map) { }
   
   ngOnInit(): void {
     this.injectMap();
   }
+
+  onMapTabChanged(tab: TabComponent) {
+    this.mapSelectedTabIndex = tab.index;
+    switch (tab.index) {
+        // case 0:
+        //     this.plotMotoristLocations();
+        //     break;
+        // case 1:
+        //     this.plotHistoryLocations();
+        //     break;
+    }
+}
+
+onMapTabSelected(tab: TabComponent) {
+    this.mapTabsSelectedIndex = tab.index;
+}
 
   private injectMap(): void {
     this.map.createMapBoxMapInstance(this.mapSelector.nativeElement);
