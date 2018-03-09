a2f63ab3c35e865d2ef50b0ff1b863cf46a09746
Elton Ventura <elton.ea@live.com>
2018-03-08 22:29:49 -0300

    feat: add marker on map

--- src/app/motorist/map-motorist/map-motorist.component.ts
+++ src/app/motorist/map-motorist/map-motorist.component.ts
@@ -4,6 +4,7 @@ import {} from 'leaflet-marker-cluster';
 
 import { Map } from '../../shared/models/Map';
 import { TabComponent } from '../../shared/components/tabs/tab/tab.component';
+import { Feature, GeometryObject } from 'geojson';
 
 @Component({
   selector: 'sga-map-motorist',
@@ -11,8 +12,63 @@ import { TabComponent } from '../../shared/components/tabs/tab/tab.component';
   styleUrls: ['./map-motorist.component.scss']
 })
 export class MapMotoristComponent implements OnInit {
+  private _motorists: Array<any>;
+  private mapMarkers: Array<Feature<GeometryObject>> = [];
+
   @ViewChild('mapSelector') mapSelector: ElementRef;
-  @Input('motorists') motorists = new Array();
+  @Input()
+  get motorists(): Array<any> {
+    return this._motorists;
+  }
+
+  set motorists(value: Array<any>) {
+    this._motorists = value;
+    if (this.mapSelectedTabIndex && this.mapSelectedTabIndex != 1) {
+      this.plotMotoristLocations();
+    }
+  }
+
+  plotMotoristLocations(): any {
+    this.map.clearAll();
+    this.mapMarkers = [];
+    this._motorists.forEach(motorist => {
+      if (motorist.location) {
+        const markerElement: HTMLElement = document.createElement('div');
+        const markerBody: HTMLElement = document.createElement('div');
+        const imageElement: HTMLImageElement = document.createElement('img');
+
+        imageElement.className = 'motorist-marker-image';
+        imageElement.src = 'api/motorist/public/profileImage?id=' + motorist.id;
+
+        markerElement.appendChild(markerBody);
+        markerBody.appendChild(imageElement);
+        markerBody.className = 'motorist-marker bounce';
+
+        markerBody.addEventListener('click', () =>
+          window.alert(motorist.firstName + ' ' + motorist.lastName)
+        );
+
+        const marker = <Feature<any>>{
+          type: 'Feature',
+          properties: { iconSize: [50, 50], icon: markerElement },
+          geometry: {
+            type: 'Point',
+            coordinates: [
+              motorist.location.longitude,
+              motorist.location.latitude
+            ]
+          }
+        };
+
+        const customMarker = this.map.createMarker(marker);
+        marker.properties['marker'] = customMarker;
+        this.mapMarkers.push(marker);
+      }
+    });
+
+    // Add cluster
+    this.map.addCluster(this.mapMarkers);
+  }
 
   mapLocationHistory = new Array();
 
@@ -31,12 +87,12 @@ export class MapMotoristComponent implements OnInit {
   onMapTabChanged(tab: TabComponent) {
     this.mapSelectedTabIndex = tab.index;
     switch (tab.index) {
-    // case 0:
-    //     this.plotMotoristLocations();
-    //     break;
-    // case 1:
-    //     this.plotHistoryLocations();
-    //     break;
+    case 0:
+        this.plotMotoristLocations();
+        break;
+    case 1:
+        //this.plotHistoryLocations();
+        break;
     }
   }
 
@@ -62,5 +118,6 @@ export class MapMotoristComponent implements OnInit {
 
   private injectMap(): void {
     this.map.createMapBoxMapInstance(this.mapSelector.nativeElement);
+    this.plotMotoristLocations();
   }
 }
