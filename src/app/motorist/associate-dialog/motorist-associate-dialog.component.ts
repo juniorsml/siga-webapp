478ce440bf93ac4e43c99fd752da806d543761c7
Elton Ventura <elton.ea@live.com>
2018-03-16 13:33:56 -0300

    refactor: rewrite methods to improve perform

--- src/app/motorist/associate-dialog/motorist-associate-dialog.component.ts
+++ src/app/motorist/associate-dialog/motorist-associate-dialog.component.ts
@@ -3,34 +3,42 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'sga-motorist-associate-dialog',
   templateUrl: './motorist-associate-dialog.component.html',
-  styles: []
+  styleUrls: ['./motorist-associate-dialog.component.scss']
 })
 export class MotoristAssociateDialogComponent implements OnInit {
-  hideAdminErrorModal: false;
   @Input() showDialog: boolean;
-  @Input() motorists: any;
-
-  currentList: any;
-
-  constructor() { }
-
-  ngOnInit() {
+  @Input()
+  get motorists(): Array<any> {
+    return this._motorists;
+  }
+  set motorists(motorists: Array<any>) {
+    this._motorists = motorists;
+    this.setCurrentMotorists();
   }
 
-  addList() {
+  private _motorists: Array<any> = [];
 
+  public removeList: Array<any> = [];
+  public currentList: Array<any> = [];
+  public hideAdminErrorModal: false;
+  public searchText: any;
+
+  constructor() {}
+
+  setCurrentMotorists() {
+    this.currentList = [];
+    this.motorists.map(motorist => {
+      if (!this.motoristIsDuplicate(motorist.id)) this.currentList.push(motorist)
+    });
   }
 
-  removeList() {
-
+  motoristIsDuplicate(motoristId): boolean {
+    return this.removeList.find(m => m.id == motoristId);
   }
 
-  searchText() {
+  ngOnInit() {}
 
-  }
+  addList() {}
 
-  applyChanges() {
-    
-  }
-
+  applyChanges() {}
 }
