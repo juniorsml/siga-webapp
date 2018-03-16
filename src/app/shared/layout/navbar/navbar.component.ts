54832f34c7b8d8e00be11f95af4d5b029f430712
Lincoli Xavier Θ_Θ <lincolixavier@hotmail.com>
2018-03-16 16:23:08 -0300

    feat: add toggle click fullscreen

--- src/app/shared/layout/navbar/navbar.component.ts
+++ src/app/shared/layout/navbar/navbar.component.ts
@@ -6,7 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: [ './navbar.component.scss' ]
 })
 export class NavbarComponent implements OnInit {
-  toggleFullScreen: any;
+	
+  toggleFullScreen() {
+        var doc: any = document;
+        var docEl: any = doc.documentElement;
+
+        if (doc.fullscreenElement || doc.webkitFullscreenElement || doc.mozFullScreenElement || doc.msFullscreenElement) {
+            if (doc.exitFullscreen) {
+                doc.exitFullscreen();
+            } else if (doc.mozCancelFullScreen) {
+                doc.mozCancelFullScreen();
+            } else if (doc.webkitExitFullscreen) {
+                doc.webkitExitFullscreen();
+            }
+        } else {
+            if (docEl.requestFullscreen) {
+                docEl.requestFullscreen();
+            } else if (docEl.mozRequestFullScreen) {
+                docEl.mozRequestFullScreen();
+            } else if (docEl.webkitRequestFullscreen) {
+                docEl.webkitRequestFullscreen();
+            } else if (docEl.msRequestFullscreen) {
+                docEl.msRequestFullscreen();
+            }
+        }
+
+        if (doc.fullscreenElement || doc.webkitFullscreenElement) {
+            if (doc.exitFullscreen) {
+                doc.exitFullscreen();
+            }
+        } else {
+            if (docEl.requestFullscreen) {
+                docEl.requestFullscreen();
+            }
+        }
+    };
 
   constructor() { }
 
