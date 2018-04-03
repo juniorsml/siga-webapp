278027433ce4a79d4b4fa26c75f953d768f1d318
Elton Ventura <elton.ea@live.com>
2018-04-03 15:02:20 -0300

    feat: add nested component to render childs by select

--- src/app/trip/trip.component.ts
+++ src/app/trip/trip.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
 import { StepClickEvent } from '../shared/events/StepClickEvent';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'sga-trip',
   templateUrl: './trip.component.html'
 })
 export class TripComponent {
+  constructor(private router: Router) { }
+  
   onSelectStep(event: StepClickEvent) {
     switch (event.data.header) {
       case 'Geral': 
+        this.router.navigateByUrl('trip/detail');
         break;
+        
+      case 'Itinerário':
+        this.router.navigateByUrl('trip/places');
+        break;
+      
+      case 'Motorista':
+        this.router.navigateByUrl('trip/motorist');
+        break;
+
       case 'Viagem':
         break; 
     }
