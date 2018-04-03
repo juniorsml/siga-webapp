967e585a78db32dbaa7b6851dc0c7686195d0256
Elton Ventura <elton.ea@live.com>
2018-04-03 15:02:32 -0300

    chore: add new routes

--- src/app/trip/trip-routing.module.ts
+++ src/app/trip/trip-routing.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { TripComponent } from './trip.component';
+import { DetailComponent } from './detail/detail.component';
+import { PlacesComponent } from './places/places.component';
+import { MotoristComponent } from './motorist/motorist.component';
 
 const routes: Routes = [
-  { path: '', component: TripComponent }
+  {
+    path: '',
+    component: TripComponent,
+    children: [
+      { path: 'detail', component: DetailComponent },
+      { path: 'places', component: PlacesComponent },
+      { path: 'motorist', component: MotoristComponent }
+    ]
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class TripRoutingModule { }
+export class TripRoutingModule {}
