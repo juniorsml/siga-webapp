80fd972b4a9e293ef25d9b20e731239e8264e68c
Elton Ventura <elton.ea@live.com>
2018-04-03 15:02:44 -0300

    feat: import motorist module

--- src/app/trip/trip.module.ts
+++ src/app/trip/trip.module.ts
@@ -4,13 +4,18 @@ import { CommonModule } from '@angular/common';
 import { TripRoutingModule } from './trip-routing.module';
 import { TripComponent } from './trip.component';
 import { SharedModule } from '../shared/modules/shared.module';
+import { DetailComponent } from './detail/detail.component';
+import { PlacesComponent } from './places/places.component';
+import { MotoristModule } from '../motorist/motorist.module';
+import { MotoristComponent } from './motorist/motorist.component';
 
 @NgModule({
   imports: [
     SharedModule,
     CommonModule,
+    MotoristModule,
     TripRoutingModule
   ],
-  declarations: [TripComponent]
+  declarations: [TripComponent, DetailComponent, PlacesComponent, MotoristComponent]
 })
 export class TripModule { }
