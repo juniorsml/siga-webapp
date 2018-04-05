147c9a1e39885fa07177b2bc66d6484ad6a23c9a
Elton Ventura <elton.ea@live.com>
2018-04-05 14:05:29 -0300

    feat: add segmented routes

--- src/app/motorist/motorist-routing.module.ts
+++ src/app/motorist/motorist-routing.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { MotoristComponent } from './motorist.component';
+import { MapMotoristComponent } from './map-motorist/map-motorist.component';
+import { GridMotoristComponent } from './grid-motorist/grid-motorist.component';
+import { MotoristAssociateDialogComponent } from './associate-dialog/motorist-associate-dialog.component';
+
+import { motorists } from '../shared/mocks/motorist';
 
 const routes: Routes = [
-  { path: '', component: MotoristComponent }
+  { path: 'map', component: MapMotoristComponent, data: { motorists: motorists } },
+  { path: 'grid', component: GridMotoristComponent, data: { motorists } },
+  { path: 'account', component: MotoristAssociateDialogComponent, data: { motorists } }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class MotoristRoutingModule { }
-
-
+export class MotoristRoutingModule {}
