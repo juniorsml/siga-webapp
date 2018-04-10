091402c297b16d388ae8a263d57e780ae2a9c23f
Lincoli Xavier <lincolixavier@gmail.com>
2018-04-10 17:36:47 -0300

    feat: create sidebar-configuration component

---
+++ src/app/shared/components/sidebar-configuration/sidebar-configuration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sga-sidebar-configuration',
+  templateUrl: './sidebar-configuration.component.html',
+  styleUrls: ['./sidebar-configuration.component.scss']
+})
+export class SidebarConfigurationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
