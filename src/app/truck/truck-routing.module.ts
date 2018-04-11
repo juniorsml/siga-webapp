89921e83d6f10917ce32ff3dab3fdb62c64166e5
Elton Ventura <elton.ea@live.com>
2018-04-11 16:28:12 -0300

    chore: add routes in truck module

--- src/app/truck/truck-routing.module.ts
+++ src/app/truck/truck-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MapTruckComponent } from './map-truck/map-truck.component';
+import { GridTruckComponent } from './grid-truck/grid-truck.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'map',
+    component: MapTruckComponent
+  },
+  {
+    path: 'grid',
+    component: GridTruckComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/truck/truck.module.ts
+++ src/app/truck/truck.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TruckRoutingModule } from './truck-routing.module';
+import { GridTruckComponent } from './grid-truck/grid-truck.component';
+import { MapTruckComponent } from './map-truck/map-truck.component';
 
 @NgModule({
   imports: [
     CommonModule,
     TruckRoutingModule
   ],
-  declarations: []
+  declarations: [GridTruckComponent, MapTruckComponent]
 })
 export class TruckModule { }
