f64c3efb52456355073ea179513209f9fe4c6503
Elton Ventura <elton.ea@live.com>
2018-04-11 16:28:20 -0300

    chore: add routes in vehicle module

--- src/app/vehicle/vehicle-routing.module.ts
+++ src/app/vehicle/vehicle-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MapVehicleComponent } from './map-vehicle/map-vehicle.component';
+import { GridVehicleComponent } from './grid-vehicle/grid-vehicle.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'map',
+    component: MapVehicleComponent
+  },
+  {
+    path: 'grid',
+    component: GridVehicleComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
