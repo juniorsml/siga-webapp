e394385d898503c46f0563eef69e6284795e3cf5
Elton Ventura <elton.ea@live.com>
2018-04-11 16:07:07 -0300

    feat: add truck module

---
+++ src/app/truck/truck.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TruckRoutingModule } from './truck-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    TruckRoutingModule
+  ],
+  declarations: []
+})
+export class TruckModule { }
