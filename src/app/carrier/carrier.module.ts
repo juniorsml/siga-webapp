93e61cbccb6963202ce37c48edf86446d02f2920
Elton Ventura <elton.ea@live.com>
2018-04-11 16:07:38 -0300

    feat: add carrier module

---
+++ src/app/carrier/carrier.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CarrierRoutingModule } from './carrier-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    CarrierRoutingModule
+  ],
+  declarations: []
+})
+export class CarrierModule { }
