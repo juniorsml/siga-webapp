12116734a96fef5fffbbdaa8d4b9afd10590d837
Elton Ventura <elton.ea@live.com>
2018-04-11 16:26:31 -0300

    feat: add  map device component

---
+++ src/app/device/map-device/map-device.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sga-map-device',
+  templateUrl: './map-device.component.html',
+  styles: []
+})
+export class MapDeviceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
