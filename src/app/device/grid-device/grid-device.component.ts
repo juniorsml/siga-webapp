0dd8a65038be078c4b19c2df6924fea632f5d42c
Elton Ventura <elton.ea@live.com>
2018-04-11 16:26:46 -0300

    feat: add  grid device component

---
+++ src/app/device/grid-device/grid-device.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sga-grid-device',
+  templateUrl: './grid-device.component.html',
+  styles: []
+})
+export class GridDeviceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
