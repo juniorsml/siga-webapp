2bdc48f60e662c90bdaada6e64b555540fd5b1d4
Elton Ventura <elton.ea@live.com>
2018-04-11 16:25:39 -0300

    feat: add  map vehicle component

---
+++ src/app/vehicle/map-vehicle/map-vehicle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sga-map-vehicle',
+  templateUrl: './map-vehicle.component.html',
+  styles: []
+})
+export class MapVehicleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
