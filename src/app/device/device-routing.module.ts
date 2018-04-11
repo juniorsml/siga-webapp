347f7b70423479eb531bebb1004e507c8532e322
Elton Ventura <elton.ea@live.com>
2018-04-11 16:28:05 -0300

    chore: add routes in device module

--- src/app/device/device-routing.module.ts
+++ src/app/device/device-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MapDeviceComponent } from './map-device/map-device.component';
+import { GridDeviceComponent } from './grid-device/grid-device.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'map',
+    component: MapDeviceComponent
+  },
+  {
+    path: 'grid',
+    component: GridDeviceComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/device/device.module.ts
+++ src/app/device/device.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DeviceRoutingModule } from './device-routing.module';
+import { GridDeviceComponent } from './grid-device/grid-device.component';
+import { MapDeviceComponent } from './map-device/map-device.component';
 
 @NgModule({
   imports: [
     CommonModule,
     DeviceRoutingModule
   ],
-  declarations: []
+  declarations: [GridDeviceComponent, MapDeviceComponent]
 })
 export class DeviceModule { }
