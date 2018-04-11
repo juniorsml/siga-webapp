77cbf313e9beda2c0cc233ffb2be6ceb64f9bd6b
Elton Ventura <elton.ea@live.com>
2018-04-11 16:07:22 -0300

    feat: add device module

---
+++ src/app/device/device.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DeviceRoutingModule } from './device-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    DeviceRoutingModule
+  ],
+  declarations: []
+})
+export class DeviceModule { }
