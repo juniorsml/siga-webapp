2c6bcd8b96d934bbd069e8fbc298c5528aac6f17
Elton Ventura <elton.ea@live.com>
2018-04-11 16:27:46 -0300

    chore: add routes in carrier module

--- src/app/carrier/carrier-routing.module.ts
+++ src/app/carrier/carrier-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MapCarrierComponent } from './map-carrier/map-carrier.component';
+import { GridCarrierComponent } from './grid-carrier/grid-carrier.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'map',
+    component: MapCarrierComponent
+  },
+  {
+    path: 'grid',
+    component: GridCarrierComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/carrier/carrier.module.ts
+++ src/app/carrier/carrier.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CarrierRoutingModule } from './carrier-routing.module';
+import { GridCarrierComponent } from './grid-carrier/grid-carrier.component';
+import { MapCarrierComponent } from './map-carrier/map-carrier.component';
 
 @NgModule({
   imports: [
     CommonModule,
     CarrierRoutingModule
   ],
-  declarations: []
+  declarations: [GridCarrierComponent, MapCarrierComponent]
 })
 export class CarrierModule { }
