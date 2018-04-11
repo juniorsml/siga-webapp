79370c58f9b86312f764f5278377af76ac92f793
Elton Ventura <elton.ea@live.com>
2018-04-11 16:26:19 -0300

    feat: add  grid truck component

---
+++ src/app/truck/grid-truck/grid-truck.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sga-grid-truck',
+  templateUrl: './grid-truck.component.html',
+  styles: []
+})
+export class GridTruckComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
