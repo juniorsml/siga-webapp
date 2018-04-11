c0b17bcb95c2488d61e1c3971934746fe503f9bd
Elton Ventura <elton.ea@live.com>
2018-04-11 16:25:51 -0300

    feat: add grid vehicle component

---
+++ src/app/vehicle/grid-vehicle/grid-vehicle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sga-grid-vehicle',
+  templateUrl: './grid-vehicle.component.html',
+  styles: []
+})
+export class GridVehicleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
