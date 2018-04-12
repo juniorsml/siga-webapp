57712130075fe54af67c40d0e6a652bbfb483d25
Elton Ventura <elton.ea@live.com>
2018-04-12 16:47:38 -0300

    feat: add Map provider to bind mapservice

--- src/app/device/device.module.ts
+++ src/app/device/device.module.ts
@@ -1,15 +1,29 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 import { DeviceRoutingModule } from './device-routing.module';
+import { SharedModule } from '../shared/modules/shared.module';
+
 import { GridDeviceComponent } from './grid-device/grid-device.component';
 import { MapDeviceComponent } from './map-device/map-device.component';
+import { MapService } from '../shared/services/map.service';
+
+import { Map } from '../shared/models/Map';
 
 @NgModule({
   imports: [
+    FormsModule,
+    SharedModule,
     CommonModule,
     DeviceRoutingModule
   ],
-  declarations: [GridDeviceComponent, MapDeviceComponent]
+  declarations: [GridDeviceComponent, MapDeviceComponent],
+  providers: [
+    {
+      provide: Map,
+      useClass: MapService
+    }
+  ]
 })
 export class DeviceModule { }
