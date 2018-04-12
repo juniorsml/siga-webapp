af325890f69f14482ae5f5441a2b6c8e84a60fbb
Elton Ventura <elton.ea@live.com>
2018-04-12 16:46:43 -0300

    feat: add device mock

---
+++ src/app/shared/mocks/device.ts
@@ -0,0 +1,22 @@
+export const devices = [
+  {
+    id: '1ASFAAG977',
+    tech: '3425HTR',
+    model: 'Fiat', 
+    alias: 'old',
+    location: {
+      latitude: -23.5076046,
+      longitude: -46.5665882
+    }
+  },
+  {
+    id: '9ASFBBG923',
+    tech: '7775EBB',
+    model: 'Ford', 
+    alias: 'new',
+    location: {
+      latitude: -23.5076046,
+      longitude: -46.5665882
+    }
+  }
+];
