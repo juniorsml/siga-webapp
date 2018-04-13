8690f3ee35797ad4f6e4eb281489e077b860df47
Elton Ventura <elton.ea@live.com>
2018-04-13 20:29:52 -0300

    chore: import device module

--- src/app/trip/trip.module.ts
+++ src/app/trip/trip.module.ts
@@ -1,22 +1,32 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
-import { TripRoutingModule } from './trip-routing.module';
-import { TripComponent } from './trip.component';
-import { SharedModule } from '../shared/modules/shared.module';
-import { DetailComponent } from './detail/detail.component';
-import { PlacesComponent } from './places/places.component';
+import { DeviceModule } from '../device/device.module';
 import { MotoristModule } from '../motorist/motorist.module';
+
+import { SharedModule } from '../shared/modules/shared.module';
+import { TripRoutingModule } from './trip-routing.module';
+
+import { TripComponent } from './trip.component';
+import { DetailComponent } from './detail/detail.component';
+import { DeviceComponent } from './device/device.component';
+import { PlacesComponent } from './places/places.component';
 import { MotoristComponent } from './motorist/motorist.component';
 import { VehiclesComponent } from './vehicles/vehicles.component';
 
+
 @NgModule({
   imports: [
+    FormsModule,
     SharedModule,
     CommonModule,
+
+    DeviceModule,
     MotoristModule,
+
     TripRoutingModule
   ],
-  declarations: [TripComponent, DetailComponent, PlacesComponent, MotoristComponent, VehiclesComponent]
+  declarations: [TripComponent, DetailComponent, PlacesComponent, MotoristComponent, VehiclesComponent, DeviceComponent]
 })
 export class TripModule { }
