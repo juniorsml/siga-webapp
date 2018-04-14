2860636f930b026235a544568eb93070bb53b7c9
Lincoli Xavier <lincolixavier@gmail.com>
2018-04-14 20:05:24 -0300

    chore: add Dom Handler to shared folder

--- src/app/vehicle/vehicle.module.ts
+++ src/app/vehicle/vehicle.module.ts
@@ -9,7 +9,7 @@ import { GridVehicleComponent } from './grid-vehicle/grid-vehicle.component';
 import { DialogVehicleComponent } from './dialog-vehicle/dialog-vehicle.component';
 
 import { MapService } from '../shared/services/map.service';
-import { DomHandler } from './dom-handler/domhandler.service';
+import { DomHandler } from '../shared/dom-handler/domhandler.service';
 
 import { Map } from '../shared/models/Map';
 
