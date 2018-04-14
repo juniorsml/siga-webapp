c67023c59c2877a9adf5d281ff818832ca3bcab4
Lincoli Xavier <lincolixavier@gmail.com>
2018-04-14 19:14:14 -0300

    chore: add imports to vehicle map

--- src/app/vehicle/map-vehicle/map-vehicle.component.ts
+++ src/app/vehicle/map-vehicle/map-vehicle.component.ts
@@ -1,9 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Component, Input, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+import {} from 'leaflet-marker-cluster';
+
+import { Map } from '../../shared/models/Map';
+import { TabComponent } from '../../shared/components/tabs/tab/tab.component';
+import { Feature, GeometryObject } from 'geojson';
+import { TableClickEvent } from '../../shared/components/table/table.component';
+import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'sga-map-vehicle',
   templateUrl: './map-vehicle.component.html',
-  styles: []
+  styleUrls: ['./map-vehicle.component.scss',]
 })
 export class MapVehicleComponent implements OnInit {
 
