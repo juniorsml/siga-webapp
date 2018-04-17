40d33e0f623e7c051540444c7cca87a2853bd50d
Elton Ventura <elton.ea@live.com>
2018-04-17 15:15:07 -0300

    feat: add point component

---
+++ src/app/geographic/point/point.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sga-point',
+  templateUrl: './point.component.html',
+  styles: []
+})
+export class PointComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
