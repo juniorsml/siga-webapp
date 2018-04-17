17cdc5b0460d2222d6677779128a55b50bd62f8c
Elton Ventura <elton.ea@live.com>
2018-04-17 14:42:49 -0300

    feat: add autocomplete and form register to device

--- src/app/trip/device/device.component.ts
+++ src/app/trip/device/device.component.ts
@@ -7,12 +7,34 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['device.component.scss']
 })
 export class DeviceComponent implements OnInit {
-
   public devices: Array<any>;
+  public associateDevice = new Array<any>();
+
+  public showRegisterForm = false;
 
   constructor(private router: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.router.data.subscribe(data => (this.devices = data.devices || new Array<any>()));
+    this.router.data.subscribe(
+      data => (this.devices = data.devices || new Array<any>())
+    );
+  }
+
+  public showDeviceForm(event): void {
+    event;
+    this.showRegisterForm = true;
+  }
+
+  public closeFormRegister() {
+    this.showRegisterForm = false;
+  }
+
+  public onDeviceSelected(device: any): void {
+    this.associateDevice.push(device);
+  }
+
+  public removeAssociate(device: any): void {
+    const index = this.associateDevice.findIndex(a => a === device);
+    this.associateDevice.splice(index, 1);
   }
 }
