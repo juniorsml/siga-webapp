71659a29a6e9852ca6c351b8160c4d0e2c5c8a48
Elton Ventura <elton.ea@live.com>
2018-04-17 15:14:58 -0300

    feat: add entity component

---
+++ src/app/geographic/entity/entity.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sga-entity',
+  templateUrl: './entity.component.html',
+  styles: []
+})
+export class EntityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
