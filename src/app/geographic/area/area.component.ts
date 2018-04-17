9d1a147cc6af8ec6839092827782042eae968061
Elton Ventura <elton.ea@live.com>
2018-04-17 15:14:47 -0300

    feat: add area component

---
+++ src/app/geographic/area/area.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sga-area',
+  templateUrl: './area.component.html',
+  styles: []
+})
+export class AreaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
