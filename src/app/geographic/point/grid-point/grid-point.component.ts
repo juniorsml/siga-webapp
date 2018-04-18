2af802be7e7013cab6028969383cb65addef7d35
Elton Ventura <elton.ea@live.com>
2018-04-18 20:41:25 -0300

    feat: add grid point component

---
+++ src/app/geographic/point/grid-point/grid-point.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sga-grid-point',
+  templateUrl: './grid-point.component.html',
+  styles: []
+})
+export class GridPointComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
