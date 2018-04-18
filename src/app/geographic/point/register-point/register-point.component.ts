aa09d0c6abf9829b8e0fdb2a69fe8c3173d619b4
Elton Ventura <elton.ea@live.com>
2018-04-18 20:41:05 -0300

    feat: add register area component

---
+++ src/app/geographic/point/register-point/register-point.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sga-register-point',
+  templateUrl: './register-point.component.html',
+  styles: []
+})
+export class RegisterPointComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
