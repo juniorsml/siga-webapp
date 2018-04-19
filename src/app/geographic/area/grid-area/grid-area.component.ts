4bb6a0abaade3dfd24089686c58794fea9d8fa21
Elton Ventura <elton.ea@live.com>
2018-04-19 12:05:33 -0300

    feat: add map-filter to grid

--- src/app/geographic/area/grid-area/grid-area.component.ts
+++ src/app/geographic/area/grid-area/grid-area.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { areas } from '../../../shared/mocks/area';
 
 @Component({
   selector: 'sga-grid-area',
   templateUrl: './grid-area.component.html',
-  styles: []
+  styleUrls: ['./grid-area.component.scss']
 })
-export class GridAreaComponent {
+export class GridAreaComponent implements OnInit {
+
+  public text: any;
+
+  public data = new Array<any>();
 
   public onPlacesFiltered(event): void {
     event
@@ -13,5 +18,17 @@ export class GridAreaComponent {
 
   public onPlacesFilterRemoved(): void {
 
+  }
+
+  public onCellClick(event) {
+    event
+  }
+
+  public onCellRightClick(event) {
+    event
+  }
+
+  ngOnInit(): void {
+    this.data = areas;
   }
 }
