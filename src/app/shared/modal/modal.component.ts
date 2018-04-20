d5649542f54ca827200ab2b897df920b1183ff26
Lincoli Xavier <lincolixavier@gmail.com>
2018-04-20 19:45:51 -0300

    style: datable improved styles

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, transition, style, animate } from '@angular/animations'
+
 
 @Component({
   selector: 'sga-modal',
   templateUrl: './modal.component.html',
-  styles: []
+  animations: [
+    trigger('visibilityChanged', [
+      state('shown', style({ opacity: 1 })),
+      state('hidden', style({ opacity: 0 })),
+      transition('shown => hidden', animate('600ms')),
+      transition('hidden => shown', animate('300ms')),
+    ])
+  ]
 })
-export class ModalComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class ModalComponent {
+  visiblityState = 'hidden'
+  toggle() {
+    if (this.visiblityState === 'hidden')
+      this.visiblityState = 'shown'
+    else
+      this.visiblityState = 'hidden'
   }
-
 }
