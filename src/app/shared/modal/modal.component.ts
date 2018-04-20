147efa1c423eaf72dc887231f59d32f71a2a3c89
Lincoli Xavier <lincolixavier@gmail.com>
2018-04-20 20:47:26 -0300

    style: datatable improved styles associate-motorist

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,25 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state, transition, style, animate } from '@angular/animations'
-
 
 @Component({
   selector: 'sga-modal',
   templateUrl: './modal.component.html',
-  animations: [
-    trigger('visibilityChanged', [
-      state('shown', style({ opacity: 1 })),
-      state('hidden', style({ opacity: 0 })),
-      transition('shown => hidden', animate('600ms')),
-      transition('hidden => shown', animate('300ms')),
-    ])
-  ]
+  styles: []
 })
-export class ModalComponent {
-  visiblityState = 'hidden'
-  toggle() {
-    if (this.visiblityState === 'hidden')
-      this.visiblityState = 'shown'
-    else
-      this.visiblityState = 'hidden'
+export class ModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
   }
+
 }
