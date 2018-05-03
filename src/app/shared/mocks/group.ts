4def15973526d8dbbb76894935121fa0152ac401
Elton Ventura <elton.ea@live.com>
2018-05-03 12:42:51 -0300

    chore: add group mock

---
+++ src/app/shared/mocks/group.ts
@@ -0,0 +1,40 @@
+export const groups = [
+  {
+    id: 1,
+    name: 'Postos Fiscais',
+    location: [
+      {
+        latitude: -23.5076046,
+        longitude: -46.5665882
+      },
+      {
+        latitude: -23.5076046,
+        longitude: -46.5665882
+      },
+      {
+        latitude: -23.936890, 
+        longitude: -46.276543
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: 'Restaurantes',
+    location: [
+      {
+        latitude: -23.5076046,
+        longitude: -46.5665882
+      }
+    ]
+  },
+  {
+    id: 3,
+    name: 'Paradas',
+    location: [
+      {
+        latitude: -23.936890, 
+        longitude: -46.276543
+      }
+    ] 
+  }
+];
