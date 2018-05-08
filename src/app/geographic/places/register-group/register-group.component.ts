4f66d19f55da169d68f37217334687464d69053b
Elton Ventura <elton.ea@live.com>
2018-05-08 18:06:48 -0300

    feat: add new events to register-group component

--- src/app/geographic/places/register-group/register-group.component.ts
+++ src/app/geographic/places/register-group/register-group.component.ts
@@ -10,17 +10,41 @@ export class RegisterGroupComponent {
 
   @Output() public onBackButton = new EventEmitter<void>();
   @Output() public onPlaceSelected = new EventEmitter<any>();
+  @Output() public onRegisterNewItem = new EventEmitter<any>();
+
+  public showRegister: boolean;
 
   public backButton() {
-    this.onBackButton.emit();
+    if (this.showRegister)
+      this.showRegister = false;
+    else
+      this.onBackButton.emit();
   }
 
   public onSelected(event) {
-    this.onPlaceSelected.emit({
-      location: {
-        latitude: event.latitude,
-        longitude: event.longitude
-      }
-    });
+    if (event.latitude && event.longitude) {
+      this.onPlaceSelected.emit({
+        location: {
+          latitude: event.latitude,
+          longitude: event.longitude
+        }
+      });
+    } else {
+      this.onPlaceSelected.emit({
+        location: {
+          latitude: event.lat(),
+          longitude: event.lng()
+        }
+      });
+    }
+  }
+
+  public registerNewItem(item) {
+    this.showRegister = false;
+    this.onRegisterNewItem.emit({ groupName: this.group.name, item });
+  }
+
+  public showCreateNew() {
+    this.showRegister = true;
   }
 }
