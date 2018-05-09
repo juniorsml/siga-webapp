e5ece63eeb05dce1ece7299f277c154869ba4908
Lincoli Xavier <lincolixavier@gmail.com>
2018-05-09 12:54:59 -0300

    chore: add riskmanagent module ts

---
+++ src/app/risk-management/risk-management.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { DeviceModule } from '../device/device.module';
+import { MotoristModule } from '../motorist/motorist.module';
+import { VehicleModule } from '../vehicle/vehicle.module';
+import { TruckModule } from '../truck/truck.module';
+
+import { SharedModule } from '../shared/modules/shared.module';
+
+
+@NgModule({
+  imports: [
+    FormsModule,
+    SharedModule,
+    CommonModule,
+
+    DeviceModule,
+    MotoristModule,
+    VehicleModule,
+    TruckModule
+  ],
+  declarations: [
+     
+
+  ]
+})
+export class TripModule { }
