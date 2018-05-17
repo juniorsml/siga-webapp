81c6cdfffc1c66d9f5c8d4ef52faeaafcb906af4
Elton Ventura <elton.ea@live.com>
2018-05-17 11:44:20 -0300

    feat: add map component

---
+++ src/app/shared/components/map/map.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'sga-map',
+  template: `
+    <div id="map"></div>
+    <div *ngIf="showPolygonSquare" id="calculation-box">
+      <div id="calculated-area"></div>
+    </div>
+  `,
+  styles: [`
+    #calculation-box {
+      height: 100px;
+      width: 200px;
+      position: absolute;
+      bottom: 40px;
+      left: 10px;
+      background-color: rgba(255, 255, 255, .9);
+      padding: 15px;
+      text-align: center;
+      z-index: 10;
+      border-radius: 7px;
+      transition: ease 1s;
+      opacity: 0;
+    }
+    
+    p {
+      font-family: 'Open Sans';
+      margin: 0;
+      font-size: 13px;
+    }
+  `]
+})
+export class MapComponent {
+  @Input() public showPolygonSquare: boolean;
+}
