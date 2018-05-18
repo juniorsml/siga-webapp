37008a68f0e838210b582ae9cf4032b6eec2e5d7
Lincoli Xavier <lincolixavier@gmail.com>
2018-05-18 00:46:07 -0300

    chore: add OptionClick function

--- src/app/truck/grid-truck/grid-truck.component.ts
+++ src/app/truck/grid-truck/grid-truck.component.ts
@@ -32,6 +32,15 @@ export class GridTruckComponent implements OnInit {
     this.showColumnSelector = false;
   }
 
+  onSelectOption(event: OptionClickEvent) {
+  switch (event.data.header) {      
+    case 'Seleção de Colunas': 
+      this.showColumnSelector = true;
+      break;
+    }
+  }
+
+
   constructor(private router: ActivatedRoute) { }
 
   ngOnInit(): void {
