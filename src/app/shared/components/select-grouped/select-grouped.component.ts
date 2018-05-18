3ebb68d730dee045fa75b6ea5666a6b47ed9c689
Elton Ventura <elton.ea@live.com>
2018-05-18 18:56:28 -0300

    feat: add select-grouped component

--- src/app/shared/components/select-grouped/select-grouped.component.ts
+++ src/app/shared/components/select-grouped/select-grouped.component.ts
@@ -1,11 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { GroupedItems } from './Grouped';
 
 @Component({
   selector: 'sga-select-grouped',
   templateUrl: './select-grouped.component.html',
-  styles: []
+  styleUrls: ['./select-grouped.component.scss']
 })
 export class SelectGroupedComponent {
-  @Input() public data: Array<any>;
-  @Input() public label: string;
+
+  @Input() public placeholder: string;
+  @Input() public noSuggestionsText: string;
+  @Input() public groupedItems = new Array<GroupedItems>();
+  
+  @Output() public onSelected = new EventEmitter<any>();
+  @Output() public onFiltered = new EventEmitter<string>();
+
+  @ViewChild('input') private inputElement: ElementRef;
+  
+  public hasFocus = false;
+
+  public onFocus = () => this.hasFocus = true;
+
+  public onKeyUp = event => this.onFiltered.emit(event);
+
+  public onSelect = value => {
+    this.onSelected.emit(value);
+    this.inputElement.nativeElement.value = value; 
+    this.hasFocus = false;
+  }
+
+  private 
 }
