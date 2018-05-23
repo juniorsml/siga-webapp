25755436ac718f6126b7b59aacdad5aea7dd2baa
Lincoli Xavier <lincolixavier@gmail.com>
2018-05-23 20:53:26 -0300

    feat: close outside of context box

--- src/app/shared/components/sidebar-configuration/sidebar-configuration.component.ts
+++ src/app/shared/components/sidebar-configuration/sidebar-configuration.component.ts
@@ -1,13 +1,21 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter,ElementRef } from '@angular/core';
 import { OptionComponent } from './option/option.component';
 import { OptionClickEvent } from '../../events/OptionClickEvent';
 
 @Component({
   selector: 'sga-sidebar-configuration',
   templateUrl: './sidebar-configuration.component.html',
-  styleUrls: ['./sidebar-configuration.component.scss']
+  styleUrls: ['./sidebar-configuration.component.scss'],
+  host: {
+    '(document:click)': 'outClick($event)',
+  }
+
 })
 export class SidebarConfigurationComponent {
+
+
+  
+  constructor(private _eref: ElementRef) { }
 
   @Output()
   public onSelectOption = new EventEmitter<OptionClickEvent>();
@@ -26,6 +34,15 @@ export class SidebarConfigurationComponent {
   toggleMenu() {
     this.status = !this.status;
   }
+  
+   // Close When Click outSide of Component
+   outClick(event) {
+       if (!this._eref.nativeElement.contains(event.target)){// or some similar check
+        if (this.status != false) {
+          this.status = false;
+        }
+      }
+    }
 }
 
 
--- src/app/shared/layout/navbar/navbar.component.ts
+++ src/app/shared/layout/navbar/navbar.component.ts
@@ -1,18 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ElementRef } from '@angular/core';
 
 @Component({
   selector: 'sga-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: [ './navbar.component.scss' ]
+  styleUrls: [ './navbar.component.scss' ],
+  host: {
+    '(document:click)': 'outClick($event)',
+  
 })
 export class NavbarComponent implements OnInit {
   logout: any;
+  
+  constructor(private _eref: ElementRef) { }
 
   public status: boolean = false;
 
   toggleProfileUser() {
     this.status = !this.status;
   }
+
+  // Close When Click outSide of Component
+   outClick(event) {
+       if (!this._eref.nativeElement.contains(event.target)){// or some similar check
+        if (this.status != false) {
+          this.status = false;
+        }
+      }
+    }
 	
   toggleFullScreen() {
         var doc: any = document;
@@ -49,7 +63,6 @@ export class NavbarComponent implements OnInit {
         }
     };
 
-  constructor() { }
 
   ngOnInit() {
   }
