01f8bd4c6adb2405503270b0cbd0d919e3ae89c1
Lincoli Xavier <lincolixavier@gmail.com>
2018-05-27 01:35:48 -0300

    fix: map cluster click and drag/drop

--- src/app/shared/mocks/device.ts
+++ src/app/shared/mocks/device.ts
@@ -32,4 +32,72 @@ export const devices = [
       longitude: -46.5665882
     }
   }
+  ,
+  {
+    id: '9ASFBBG923',
+    tech: '7775EBB',
+    model: 'Ford', 
+    name: 'newest',
+    distance: 9.8,
+    battery: {
+      charge: 0.5
+    },
+    updated: new Date(),
+    speed: 90,
+    location: {
+      latitude: -23.5076046,
+      longitude: -46.5665882
+    }
+  }
+  ,
+  {
+    id: '9ASFBBG923',
+    tech: '7775EBB',
+    model: 'Ford', 
+    name: 'newest',
+    distance: 9.8,
+    battery: {
+      charge: 0.5
+    },
+    updated: new Date(),
+    speed: 90,
+    location: {
+      latitude: -23.5076046,
+      longitude: -46.5665882
+    }
+  }
+  ,
+  {
+    id: '9ASFBBG923',
+    tech: '7775EBB',
+    model: 'Ford', 
+    name: 'newest',
+    distance: 9.8,
+    battery: {
+      charge: 0.5
+    },
+    updated: new Date(),
+    speed: 90,
+    location: {
+      latitude: -23.5076046,
+      longitude: -46.5665882
+    }
+  }
+  ,
+  {
+    id: '9ASFBBG923',
+    tech: '7775EBB',
+    model: 'Ford', 
+    name: 'newest',
+    distance: 9.8,
+    battery: {
+      charge: 0.5
+    },
+    updated: new Date(),
+    speed: 90,
+    location: {
+      latitude: -23.5076046,
+      longitude: -46.5665882
+    }
+  }
 ];
