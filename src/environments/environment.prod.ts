d586cc1ffbafe5ba3e8cbb83e7d7a8743cd84f1a
Elton Ventura <elton.ea@live.com>
2018-05-28 13:40:45 -0300

    chore: add directions api at environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
   production: true,
   api: '',
   mapbox: {
+    directionsApi: 'https://api.mapbox.com/directions/v5/mapbox/driving',
     accessToken: 'pk.eyJ1IjoiY29uZHVzaXQiLCJhIjoiY2oyeG1wcjJuMDExNTJ4cThlemU3NWlsNCJ9.d1o1-L4u4_-aY_uvAn5krQ',
     location: {
       latitude: -23.533773, 
