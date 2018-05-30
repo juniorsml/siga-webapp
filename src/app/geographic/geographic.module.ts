ea03ca93f047447f8db195663f4b76f62f278c0d
Elton Ventura <elton.ea@live.com>
2018-05-30 19:56:51 -0300

    refactor: remove http, map implementation from geographi

--- src/app/geographic/geographic.module.ts
+++ src/app/geographic/geographic.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 import { SharedModule } from '../shared/modules/shared.module';
 import { GeographicRoutingModule } from './geographic-routing.module';
 
-import { Map } from '../shared/models/Map';
-import { MapService } from '../shared/services/map.service';
 
 import { GridAreaComponent } from './area/grid-area/grid-area.component';
 import { RegisterAreaComponent } from './area/register-area/register-area.component';
@@ -19,10 +18,6 @@ import { RegisterComponent } from './places/register/register.component';
 import { GridControlComponent } from './places/grid-control/grid-control.component';
 import { RegisterGroupComponent } from './places/register-group/register-group.component';
 import { ModalGroupComponent } from './places/modal-group/modal-group.component';
-import { HttpService } from '../shared/services/http.service';
-import { httpFactory } from '../shared/factory/http.factory';
-import { XHRBackend, RequestOptions, HttpModule } from '@angular/http';
-import { Router } from '@angular/router';
 
 
 @NgModule({
@@ -33,21 +28,7 @@ import { Router } from '@angular/router';
     SharedModule,
     GeographicRoutingModule
   ],
-  providers: [
-    {
-      provide: HttpService,
-      useFactory: httpFactory,
-      deps: [
-        XHRBackend, 
-        RequestOptions, 
-        Router
-      ]
-    },
-    {
-      provide: Map,
-      useClass: MapService
-    }
-  ],
+  providers: [],
   declarations: [GridAreaComponent, RegisterAreaComponent, MapAreaComponent, GridPointComponent, RegisterPointComponent, MapPointComponent, RegisterPlaceComponent, RegisterComponent, GridControlComponent, RegisterGroupComponent, ModalGroupComponent]
 })
 export class GeographicModule { }
