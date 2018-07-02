fdc1e35b1f87796a1bd901db87e60f37ae1c5d87
Elton Ventura <elton.ea@live.com>
2018-07-02 16:50:07 -0300

    feat: add new way to handle steps

--- src/app/trip/register/register-trip.component.ts
+++ src/app/trip/register/register-trip.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { StepClickEvent } from '../../shared/events/StepClickEvent';
 import { Router } from '@angular/router';
+import { RegisterStepEnum } from './RegisterStepEnum';
 
 @Component({
   selector: 'sga-register-trip',
@@ -10,37 +11,52 @@ import { Router } from '@angular/router';
 export class RegisterTripComponent {
   constructor(private router: Router) { }
 
-  onSelectStep(event: StepClickEvent) {
-    switch (event.data.header) {
+  private _currentStep = 0;
+  public get currentStep(): RegisterStepEnum {
+    return this._currentStep;
+  }
+  public set currentStep(value: RegisterStepEnum) {
+    if (value >= 0 && value <= 6) {
+      this._currentStep = value;
+    }
+  }
 
-      case 'Geral':
-              this.router.navigateByUrl('trip/register/detail');
+  public next = () => this.navigateByStep(this.currentStep + 1);
+
+  public prev = () => this.navigateByStep(this.currentStep - 1);
+
+  public onSelectStep = (event: StepClickEvent) => this.navigateByStep(event.index);
+
+  private navigateByStep(step: RegisterStepEnum) {
+    this.currentStep = step;
+
+    switch (step) {
+
+      case RegisterStepEnum.General:
+        this.router.navigateByUrl('trip/register/detail');
         break;
-      
-      case 'Itinerário':
+
+      case RegisterStepEnum.Itinerary:
         this.router.navigateByUrl('trip/register/places');
         break;
-  
-      case 'Motorista':
+
+      case RegisterStepEnum.Motorist:
         this.router.navigateByUrl('trip/register/motorist');
         break;
 
-      case 'Veículos':
+      case RegisterStepEnum.Vehicle:
         this.router.navigateByUrl('trip/register/vehicles');
         break;
-      case 'Reboque':
+      case RegisterStepEnum.Truck:
         this.router.navigateByUrl('trip/register/trucks');
         break;
 
-      case 'Dispositivo':
+      case RegisterStepEnum.Device:
         this.router.navigateByUrl('trip/register/device');
         break;
 
-      case 'Resumo':
-      this.router.navigateByUrl('trip/register/summary');
-      break;
-
-      case 'Viagem':
+      case RegisterStepEnum.Summary:
+        this.router.navigateByUrl('trip/register/summary');
         break;
     }
   }
