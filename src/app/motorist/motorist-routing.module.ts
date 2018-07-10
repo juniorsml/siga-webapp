aed5d5d8b8cfd6f71ac71bc69141fd3b316476c3
Elton Ventura <elton.ea@live.com>
2018-07-10 19:34:50 -0300

    chore: motorist remove mock

--- src/app/motorist/motorist-routing.module.ts
+++ src/app/motorist/motorist-routing.module.ts
@@ -5,13 +5,12 @@ import { MapMotoristComponent } from './map-motorist/map-motorist.component';
 import { GridMotoristComponent } from './grid-motorist/grid-motorist.component';
 import { MotoristAssociateDialogComponent } from './associate-dialog/motorist-associate-dialog.component';
 
-import { motorists } from '../shared/mocks/motorist';
 import { HistoryMotoristComponent } from './history-motorist/history-motorist.component';
 
 const routes: Routes = [
-  { path: 'map', component: MapMotoristComponent, data: { motorists: motorists } },
-  { path: 'grid', component: GridMotoristComponent, data: { motorists } },
-  { path: 'account', component: MotoristAssociateDialogComponent, data: { motorists } },
+  { path: 'map', component: MapMotoristComponent },
+  { path: 'grid', component: GridMotoristComponent },
+  { path: 'account', component: MotoristAssociateDialogComponent },
   { path: 'history', component: HistoryMotoristComponent },
   { path: 'history/:id', component: HistoryMotoristComponent }
 ];
