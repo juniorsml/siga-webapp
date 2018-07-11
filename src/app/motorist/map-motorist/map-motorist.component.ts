d176ba2967cc9883e0c1a9f5392a7a3a2c8930cf
Elton Ventura <elton.ea@live.com>
2018-07-10 21:01:42 -0300

    chore: add new way to get motorists in the map service

--- src/app/motorist/map-motorist/map-motorist.component.ts
+++ src/app/motorist/map-motorist/map-motorist.component.ts
@@ -1,17 +1,18 @@
 import { Component, Input, OnInit, ViewChild, ElementRef } from '@angular/core';
-import {} from 'leaflet-marker-cluster';
+import { } from 'leaflet-marker-cluster';
 
 import { Map } from '../../shared/models/Map';
 import { TabComponent } from '../../shared/components/tabs/tab/tab.component';
 import { Feature, GeometryObject } from 'geojson';
 import { TableClickEvent } from '../../shared/components/table/table.component';
-import { ActivatedRoute } from '@angular/router';
 import { MapStyle } from '../../shared/models/MapStyle';
+import { MotoristService } from '../motorist.service';
 
 @Component({
   selector: 'sga-map-motorist',
   templateUrl: './map-motorist.component.html',
-  styleUrls: ['./map-motorist.component.scss']
+  styleUrls: ['./map-motorist.component.scss'],
+  providers: [MotoristService]
 })
 export class MapMotoristComponent implements OnInit {
   private _motorists: Array<any>;
@@ -22,7 +23,17 @@ export class MapMotoristComponent implements OnInit {
 
   public isMap: boolean;
 
+  mapLocationHistory = new Array();
+
+  mapSearchText: any;
+  mapSelectedTabIndex: number;
+  mapTabsSelectedIndex = 0;
+
+  selectedMotorist: any;
+  public status = false;
+
   @ViewChild('mapSelector') mapSelector: ElementRef;
+
   @Input()
   get motorists(): Array<any> {
     return this._motorists;
@@ -35,7 +46,14 @@ export class MapMotoristComponent implements OnInit {
     }
   }
 
-  public status = false;
+  constructor(
+    private map: Map,
+    private motoristService: MotoristService) { }
+
+  ngOnInit(): void {
+    this.getMotorists();
+    this.injectMap();
+  }
 
   toggleMapType() {
     this.status = !this.status;
@@ -44,38 +62,40 @@ export class MapMotoristComponent implements OnInit {
   plotMotoristLocations(): void {
     this.map.clearAll();
     this.mapMarkers = [];
-    this._motorists.forEach(motorist => {
-      if (motorist.location) {
-        const markerBody: HTMLElement = document.createElement('div');
-        const markerElement: HTMLElement = document.createElement('div');
-        const imageElement: HTMLImageElement = document.createElement('img');
+    if (this.motorists) {
+      this.motorists.forEach(motorist => {
+        if (motorist.location) {
+          const markerBody: HTMLElement = document.createElement('div');
+          const markerElement: HTMLElement = document.createElement('div');
+          const imageElement: HTMLImageElement = document.createElement('img');
 
-        imageElement.className = 'motorist-marker-image';
-        // imageElement.src = 'api/motorist/public/profileImage?id=' + motorist.id;
+          imageElement.className = 'motorist-marker-image';
+          // imageElement.src = 'api/motorist/public/profileImage?id=' + motorist.id;
 
-        markerElement.appendChild(markerBody);
-        markerBody.appendChild(imageElement);
-        markerBody.className = 'motorist-marker bounce';
+          markerElement.appendChild(markerBody);
+          markerBody.appendChild(imageElement);
+          markerBody.className = 'motorist-marker bounce';
 
-        markerBody.addEventListener('click', () => window.alert(motorist.firstName));
+          markerBody.addEventListener('click', () => window.alert(motorist.firstName));
 
-        const marker: Feature<GeometryObject> = <Feature<any>>{
-          type: 'Feature',
-          properties: { iconSize: [50, 50], icon: markerElement },
-          geometry: {
-            type: 'Point',
-            coordinates: [
-              motorist.location.longitude,
-              motorist.location.latitude
-            ]
-          }
-        };
+          const marker: Feature<GeometryObject> = <Feature<any>>{
+            type: 'Feature',
+            properties: { iconSize: [50, 50], icon: markerElement },
+            geometry: {
+              type: 'Point',
+              coordinates: [
+                motorist.location.longitude,
+                motorist.location.latitude
+              ]
+            }
+          };
 
-        const customMarker = this.map.createMarker(marker);
-        marker.properties['marker'] = customMarker;
-        this.mapMarkers.push(marker);
-      }
-    });
+          const customMarker = this.map.createMarker(marker);
+          marker.properties['marker'] = customMarker;
+          this.mapMarkers.push(marker);
+        }
+      });
+    }
 
     this.map.addCluster(this.mapMarkers);
   }
@@ -106,19 +126,17 @@ export class MapMotoristComponent implements OnInit {
     return marker;
   }
 
-  mapLocationHistory = new Array();
+  private getMotorists() {
+    this
+      .motoristService
+      .getMotorists()
+      .subscribe(
+        data => this.onSuccess(data),
+        error => alert(error));
+  }
 
-  mapSearchText: any;
-  mapSelectedTabIndex: number;
-  mapTabsSelectedIndex = 0;
-
-  selectedMotorist: any;
-
-  constructor(private map: Map, private router: ActivatedRoute) {}
-
-  ngOnInit(): void {
-    this.router.data.subscribe(data => this.motorists = data.motorists);
-    this.injectMap();
+  private onSuccess(data) {
+    this.motorists = data;
   }
 
   mapTableCellClick(event: TableClickEvent) {
@@ -160,8 +178,7 @@ export class MapMotoristComponent implements OnInit {
     this.map.moveTo(event.data.latitude, event.data.longitude);
   }
 
-  onPlacesFiltered(event) {
-    event;
+  onPlacesFiltered() {
   }
 
   onPlacesFilterRemoved() {
