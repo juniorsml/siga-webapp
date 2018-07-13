ef64d064b87521099ae3c2d0c8cb20fa961e2a28
Lincoli Xavier <lincolixavier@gmail.com>
2018-07-13 12:48:47 -0300

    feat: add registerSummary component

---
+++ src/app/trip/register/summary/summary-register.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+
+@Component({
+  selector: 'sga-summary-register',
+  templateUrl: './summary-register.component.html',
+  styleUrls: ['./summary-register.component.scss']
+})
+
+export class SummaryRegisterComponent {
+  @Input() public trip: any;
+}
