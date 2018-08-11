d873ac2223517c8aaa32445ef14d77838a41b39f
Lincoli Xavier <lincolixavier@gmail.com>
2018-08-10 21:23:47 -0300

    chore: add property missing sendMessage

--- src/app/shared/components/send-message/send-message.component.ts
+++ src/app/shared/components/send-message/send-message.component.ts
@@ -6,6 +6,7 @@ import { Component, EventEmitter, Output, Input } from '@angular/core';
   styleUrls: ['./send-message.component.scss']
 })
 export class SendMessageModalComponent {
+  public kindOfMessage:any;
   @Input() showModal: boolean;
 
   @Output() onDialogClose: EventEmitter<void> = new EventEmitter<void>();
