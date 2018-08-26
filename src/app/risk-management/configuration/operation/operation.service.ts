0a1447cb9862d3e828ac0f5b223361f446a5afd9
Lincoli Xavier <lincolixavier@gmail.com>
2018-08-26 10:59:50 -0300

    feat: add services operation

---
+++ src/app/risk-management/configuration/operation/operation.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs/Observable';
+import { HttpService } from '../shared/services/http.service';
+import { Operation } from '../shared/models/api/Operation';
+
+@Injectable()
+export class OperationService {
+
+  constructor(private http: HttpService) { }
+
+  public getOperations(): Observable<Array<Operation>> {
+    return this
+      .http
+      .get(`api/operations`)
+      .pipe(map(res => <Array<Operation>>res.json()));
+  }
+
+  public getOperation(operationId:string): Observable<any> {
+    return this
+      .http
+      .get(`api/operations/${operationId}`)
+      .first()
+      .pipe(map(res => res.json()));
+  }
+
+  public saveMotorist(operation: Operation): Observable<any> {
+    return this
+      .http
+      .post('api/operations', operation)
+      .first()
+      .pipe(map(res => res.json()));
+  }
+
+  public updateMotorist(operation: Operation): Observable<any> {
+    
+    return this
+      .http
+      .put(`api/operations/${operation.id}`, operation)
+      .first()
+      .pipe(map(res => res.json()));
+  }
+
+}
