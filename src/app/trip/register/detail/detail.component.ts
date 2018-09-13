da375f0e00c0d218f82858eb1405ea7cbbec8393
Lincoli Xavier <lincolixavier@gmail.com>
2018-09-13 16:42:08 -0300

    feat: call operation service to populate select

--- src/app/trip/register/detail/detail.component.ts
+++ src/app/trip/register/detail/detail.component.ts
@@ -1,6 +1,6 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, ViewChild, OnInit} from '@angular/core';
 import { FormService } from '../dataform.service';
-
+import { OperationService } from '../../../risk-management/configuration/operation/operation.service';
 
 @Component({
   selector: 'sga-detail',
@@ -8,18 +8,25 @@ import { FormService } from '../dataform.service';
   styleUrls: ['detail.component.scss']
 })
 
-export class DetailComponent {
+export class DetailComponent implements OnInit {
  
+
  @ViewChild('formDetail') formDetail: any;
  
+ listOfConfigs:any;
 
- constructor(private formData: FormService) { }
-
- 
+ constructor(private formData: FormService, private operationService: OperationService) { }
 
   ngOnDestroy(){
     this.formData.updateObj(this.formDetail.value);
     console.log(this.formDetail.value);
+  }
+
+  ngOnInit(){
+    this
+      .operationService
+      .getConfigs()
+      .subscribe(list => this.listOfConfigs = list);
   }
   
 
