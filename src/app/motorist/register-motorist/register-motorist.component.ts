34cc8052320acf2b5e2f50e26dfbd6dfb43e8101
Lincoli Xavier <lincolixavier@gmail.com>
2018-09-26 09:48:20 -0300

    chore: set ptbr birthDate datapicker

--- src/app/motorist/register-motorist/register-motorist.component.ts
+++ src/app/motorist/register-motorist/register-motorist.component.ts
@@ -67,6 +67,9 @@ export class RegisterMotoristComponent implements OnInit {
 
   public setBirthDate: IMyDpOptions = {
     dateFormat: 'dd/mm/yyyy',
+    monthLabels: { 1: 'Jan', 2: 'Fev', 3: 'Mar', 4: 'Abr', 5: 'Mai', 6: 'Jun', 7: 'Jul', 8: 'Ago', 9: 'Set', 10: 'Out', 11: 'Nov', 12: 'Dez' },
+    dayLabels: {su: 'Dom', mo: 'Seg', tu: 'Ter', we: 'Qua', th: 'Qui', fr: 'Sex', sa: 'Sáb'},
+    todayBtnTxt: 'Hoje'
    };
 
    public setDueDate: IMyDpOptions = {
